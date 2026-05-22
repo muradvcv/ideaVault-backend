a98b0e1523a9959f7633fcff2d4dbd6b50c54a34
muradvcv <muradvcv@gmail.com>
2026-05-22 12:55:02 +0600

    make route for push,get,update,delete comment

--- index.js
+++ index.js
@@ -27,11 +27,111 @@ async function run() {
   try {
 
     await client.connect();
-
     const db = client.db("ideaVault");
-
     const ideaCollection = db.collection("ideas");
+    const commentCollection = db.collection("comments")
 
+
+    // get comment from server
+    app.get('/comment', async (req, res) => {
+
+      const result = await commentCollection.find().toArray()
+      res.json(result)
+
+    })
+
+    // dleete
+    app.delete("/comment/:id", async (req, res) => {
+      const { id } = req.params;
+      const { email } = req.body;
+
+      const comment = await commentCollection.findOne({
+        _id: new ObjectId(id),
+      });
+
+      if (!comment) {
+        return res.status(404).json({ message: "Comment not found" });
+      }
+
+      // ❌ not owner
+      if (comment.userEmail !== email) {
+        return res.status(403).json({ message: "Not allowed" });
+      }
+
+      const result = await commentCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+
+      res.json(result);
+    });
+
+    app.put("/comment/:id", async (req, res) => {
+      const { id } = req.params;
+      const { email, comment } = req.body;
+
+      const existing = await commentCollection.findOne({
+        _id: new ObjectId(id),
+      });
+
+      if (!existing) {
+        return res.status(404).json({ message: "Not found" });
+      }
+
+      if (existing.userEmail !== email) {
+        return res.status(403).json({ message: "Not allowed" });
+      }
+
+      const result = await commentCollection.updateOne(
+        { _id: new ObjectId(id) },
+        {
+          $set: {
+            comment,
+          },
+        }
+      );
+
+      res.json(result);
+    });
+    // update
+
+    app.put("/comment/:id", async (req, res) => {
+      const { id } = req.params;
+      const { email, comment } = req.body;
+
+      const existing = await commentCollection.findOne({
+        _id: new ObjectId(id),
+      });
+
+      if (!existing) {
+        return res.status(404).json({ message: "Not found" });
+      }
+
+      if (existing.userEmail !== email) {
+        return res.status(403).json({ message: "Not allowed" });
+      }
+
+      const result = await commentCollection.updateOne(
+        { _id: new ObjectId(id) },
+        {
+          $set: {
+            comment,
+          },
+        }
+      );
+
+      res.json(result);
+    });
+
+    // add comment on server
+    app.post('/comment', async (req, res) => {
+      const commentData = req.body
+      const result = await commentCollection.insertOne(commentData)
+
+      res.json(result)
+
+      console.log(commentData);
+
+    })
 
 
     // get all ideas
@@ -58,7 +158,7 @@ async function run() {
 
 
 
-    // add ideas
+    // add idea
     app.post('/idea', async (req, res) => {
 
       const ideaData = req.body;
